bb82c457a2190bd95846187b3f47ffc7c43adf4e
Elgouzi oussama <gouzioussama2000@gmail.com>
2025-11-19 10:08:38 +0100

    v.01.01.05

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,36 +23,25 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::defaultStringLength(191);
 
-        // Prevent errors BEFORE migrations run
-        if (! app()->runningInConsole() || (app()->runningInConsole() && $this->isMigrating())) {
-            return;
-        }
-
-        // Run this code ONLY if the table exists
         if (Schema::hasTable('licenses')) {
-            $serverId = SystemHelper::getServerId();
-            $license = License::first();
+            $path = storage_path('app/license_init.flag');
 
-            if (! $license) {
-                License::create([
-                    'server_id' => $serverId,
-                    'auasae_'   => Carbon::now(),
-                ]);
+            if (!file_exists($path)) {
+                $serverId = SystemHelper::getServerId();
+                $license = License::first();
+
+                if (!$license) {
+                    License::create([
+                        'server_id' => $serverId,
+                        'auasae_'   => Carbon::now(),
+                    ]);
+                }
+
+                // create marker file so this block never runs again
+                file_put_contents($path, 'done');
             }
         }
-
-
-        Schema::defaultStringLength(191);
-    }
-
-
-
-
-
-    private function isMigrating()
-    {
-        $argv = $_SERVER['argv'] ?? [];
-        return in_array('migrate', $argv) || in_array('migrate:fresh', $argv);
     }
 }
