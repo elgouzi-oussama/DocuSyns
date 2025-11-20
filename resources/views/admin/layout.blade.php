4f79a687a9b8f29a88d6a7578f521b144a8db157
Elgouzi oussama <gouzioussama2000@gmail.com>
2025-11-20 16:16:52 +0100

    v.01.01.05

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', __('admin.title'))</title>
+    <link rel="icon" type="image/png" href="{{ asset('logo.png') }}">
     <script src="https://cdn.tailwindcss.com"></script>
 
 
